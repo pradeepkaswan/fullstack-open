f1fad217e5fa2eb777ee047a736fa0a2dab4be32
pradeepkaswan <pradeepkaswan7@gmail.com>
2023-10-23 00:06:04 +0530

    feat: complete 3.9 from part3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 let persons = [
 	{
@@ -99,7 +101,7 @@ app.get('/info', (request, response) => {
 	response.send(infoResponse);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
