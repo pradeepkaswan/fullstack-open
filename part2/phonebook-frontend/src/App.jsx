13ffa44f5e78dd2d75c99e9c7201c4bcdea06558
pradeepkaswan <pradeepkaswan7@gmail.com>
2023-10-22 14:44:47 +0530

    feat: complete 2.10 from part2

--- part2/phonebook-frontend/src/App.jsx
+++ part2/phonebook-frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
 
 const App = () => {
 	const [persons, setPersons] = useState([
@@ -47,39 +50,21 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<div>
-				filter shown with
-				<input
-					value={searchTerm}
-					onChange={handleSearchChange}
-				/>
-			</div>
-			<h2>add a new</h2>
-			<form onSubmit={addPerson}>
-				<div>
-					name:{' '}
-					<input
-						value={newName}
-						onChange={handleNameChange}
-					/>
-				</div>
-				<div>
-					number:{' '}
-					<input
-						value={newNumber}
-						onChange={handleNumberChange}
-					/>
-				</div>
-				<div>
-					<button type='submit'>add</button>
-				</div>
-			</form>
+			<Filter
+				searchTerm={searchTerm}
+				handleSearchChange={handleSearchChange}
+			/>
+			<h2>Add a new</h2>
+			<PersonForm
+				addPerson={addPerson}
+				newName={newName}
+				newNumber={newNumber}
+				handleNameChange={handleNameChange}
+				handleNumberChange={handleNumberChange}
+			/>
+
 			<h2>Numbers</h2>
-			{filteredPersons.map((person) => (
-				<div key={person.name}>
-					{person.name} {person.number}
-				</div>
-			))}
+			<Persons persons={filteredPersons} />
 		</div>
 	);
 };
