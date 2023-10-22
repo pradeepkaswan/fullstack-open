d8efbc1314bcfdfb32257d0814237de774f31987
pradeepkaswan <pradeepkaswan7@gmail.com>
2023-10-22 11:25:42 +0530

    feat: complete 2.2-2.5 from part2

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,24 +1,41 @@
-const Header = ({ course }) => <h1>{course.name}</h1>;
+const Header = ({ name }) => <h2>{name}</h2>;
 
-const Part = ({ part }) => (
+const Part = ({ name, exercises }) => (
 	<p>
-		{part.name} {part.exercises}
+		{name} {exercises}
 	</p>
 );
 
 const Content = ({ parts }) => (
 	<>
-		<Part part={parts[0]} />
-		<Part part={parts[1]} />
-		<Part part={parts[2]} />
+		{parts.map((part) => (
+			<Part
+				key={part.id}
+				name={part.name}
+				exercises={part.exercises}
+			/>
+		))}
 	</>
 );
+
+const Total = ({ parts }) => {
+	const total = parts.reduce((sum, part) => {
+		return sum + part.exercises;
+	}, 0);
+
+	return (
+		<p>
+			<strong>total of {total} exercises</strong>
+		</p>
+	);
+};
 
 const Course = ({ course }) => {
 	return (
 		<div>
-			<Header course={course} />
+			<Header name={course.name} />
 			<Content parts={course.parts} />
+			<Total parts={course.parts} />
 		</div>
 	);
 };
