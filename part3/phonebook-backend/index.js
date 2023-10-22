ed3657a211b171afa2c1faf584bc88538fb6968f
pradeepkaswan <pradeepkaswan7@gmail.com>
2023-10-22 21:40:20 +0530

    feat: complete 3.3-3.6 from part3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -30,6 +30,58 @@ app.get('/api/persons', (request, response) => {
 	response.json(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	const person = persons.find((person) => {
+		return person.id === id;
+	});
+
+	if (person) {
+		response.json(person);
+	} else {
+		response.status(404).end();
+	}
+});
+
+const generateId = () => {
+	const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+	return maxId + 1;
+};
+
+app.post('/api/persons', (request, response) => {
+	const body = request.body;
+
+	if (!body.name || !body.number) {
+		return response.status(400).json({
+			error: 'name or number missing',
+		});
+	}
+
+	const personExists = persons.find((person) => person.name === body.name);
+
+	if (personExists) {
+		return response.status(400).json({
+			error: 'name must be unique',
+		});
+	}
+
+	const person = {
+		id: generateId(),
+		name: body.name,
+		number: body.number,
+	};
+
+	persons = persons.concat(person);
+	response.json(person);
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	persons = persons.filter((person) => person.id !== id);
+
+	response.status(204).end();
+});
+
 app.get('/info', (request, response) => {
 	const requestTime = new Date().toString();
 	const personCount = persons.length;
