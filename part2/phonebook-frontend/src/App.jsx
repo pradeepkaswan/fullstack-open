e4a8e4f932db48f7d3d62af4d2cefab23f22b794
pradeepkaswan <pradeepkaswan7@gmail.com>
2023-10-22 14:16:29 +0530

    feat: complete 2.9 from part2

--- part2/phonebook-frontend/src/App.jsx
+++ part2/phonebook-frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+	const [persons, setPersons] = useState([
+		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+	]);
 	const [newName, setNewName] = useState('');
 	const [newNumber, setNewNumber] = useState('');
+	const [searchTerm, setSearchTerm] = useState('');
 
 	const addPerson = (event) => {
 		event.preventDefault();
@@ -22,6 +28,10 @@ const App = () => {
 		setNewNumber('');
 	};
 
+	const handleSearchChange = (event) => {
+		setSearchTerm(event.target.value);
+	};
+
 	const handleNameChange = (event) => {
 		setNewName(event.target.value);
 	};
@@ -30,22 +40,42 @@ const App = () => {
 		setNewNumber(event.target.value);
 	};
 
+	const filteredPersons = persons.filter((person) =>
+		person.name.toLowerCase().includes(searchTerm.toLowerCase()),
+	);
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<div>
+				filter shown with
+				<input
+					value={searchTerm}
+					onChange={handleSearchChange}
+				/>
+			</div>
+			<h2>add a new</h2>
 			<form onSubmit={addPerson}>
 				<div>
-					name: <input onChange={handleNameChange} />
+					name:{' '}
+					<input
+						value={newName}
+						onChange={handleNameChange}
+					/>
 				</div>
 				<div>
-					number: <input onChange={handleNumberChange} />
+					number:{' '}
+					<input
+						value={newNumber}
+						onChange={handleNumberChange}
+					/>
 				</div>
 				<div>
 					<button type='submit'>add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map((person) => (
+			{filteredPersons.map((person) => (
 				<div key={person.name}>
 					{person.name} {person.number}
 				</div>
